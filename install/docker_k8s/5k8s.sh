6bfdd649fbffe745a74967d51c8629de469c72df
doojin kim <doojin88@gmail.com>
2019-10-15 10:27:17 +0900

    update install scripts.

--- install/docker_k8s/5k8s.sh
+++
@@ -1,8 +0,0 @@
-
-curl -sfL https://get.k3s.io | sh -
-sudo chown ubuntu:ubuntu /etc/rancher/k3s/k3s.yaml
-
-kubectl get nodes
-
-cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
-
--- install/docker_k8s/7vscode.sh
+++
@@ -1,8 +0,0 @@
-
-wget https://github.com/cdr/code-server/releases/download/1.1156-vsc1.33.1/code-server1.1156-vsc1.33.1-linux-x64.tar.gz
-tar xvfz code-server1.1156-vsc1.33.1-linux-x64.tar.gz
-sudo mv code-server1.1156-vsc1.33.1-linux-x64/code-server /usr/local/bin
-mkdir ~/project
-cd ~/project
-PASSWORD=1q2w3e4r nohup code-server -p 8080 --allow-http &
-
